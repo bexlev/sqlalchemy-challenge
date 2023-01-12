5e20b7c1932e4b2b0694481a21ecfbe0b4774355
bexlev <amoebalevine@gmail.com>
2023-01-11 23:51:01 -0600

    Queries

--- app.py
+++ app.py
@@ -1,19 +1,55 @@
 # Part 2: Design Your Climate App
 
-# Now that you’ve completed your initial analysis, you’ll design a Flask API based on the queries that you just developed. To do so, use Flask to create your routes as follows:
+#Designing a Flask API based on the queries developed
 
 
 # 1. /
 
 #     Start at the homepage.
+################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
 
-#     List all the available routes.
 
+#################################################
+# Flask Routes
+#################################################
 
+#  List all the available routes.
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs
+    )
 
 # 2. /api/v1.0/precipitation
 
 #     Convert the query results from your precipitation analysis (i.e. retrieve only the last 12 months of data) to a dictionary using date as the key and prcp as the value.
+
+
+@app.route("/api/v1.0/precipitation")
+# def names():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of all passenger names"""
+#     # Query all passengers
+#     results = session.query(Passenger.name).all()
+
+#     session.close()
+
+#Import other python file with query results
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names)
 
 #     Return the JSON representation of your dictionary.
 
@@ -42,4 +78,33 @@
 #     For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
 
 
+@app.route("/api/v1.0/passengers")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_passengers = []
+    for name, age, sex in results:
+        passenger_dict = {}
+        passenger_dict["name"] = name
+        passenger_dict["age"] = age
+        passenger_dict["sex"] = sex
+        all_passengers.append(passenger_dict)
+
+    return jsonify(all_passengers)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
